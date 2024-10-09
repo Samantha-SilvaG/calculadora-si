e28c4f88f3fdbf3fe6d7bfaea3197b7abacd4199
FelipeZanatta09 <felipezanattacouto01102009@gmail.com>
2024-10-08 21:01:21 -0300

    Comprimentro
    Comprimento implementado

--- comprimento.py
+++ comprimento.py
@@ -1,4 +1,7 @@
-def comprimentro(m, km):
-    m = m // km
-    km = km * m
-    return m, km
+def conversao_comprimentroM_km(m):
+    m = m // 1000
+    return m
+
+def conversao_comprimentroKM_m(km):
+    km = km * 1000
+    return km
