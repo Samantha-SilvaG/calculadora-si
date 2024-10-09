0ba0ac41be23b34c4e3c5b0e843dbf45d144aef9
Samantha_SilvaG <Samantha.gomesdasilva@hotmail.com>
2024-10-08 21:29:01 -0300

    main

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import comprimento
+import massa
+import volume
+
+
 def main():
+    
     """
     Função principal do programa.
     """
@@ -14,8 +20,6 @@ def main():
         print("4. Massa (kg para g)")
         print("5. Volume (l para ml)")
         print("6. Volume (ml para l)")
-        print("7. Velocidade (km/h para m/s)")
-        print("8. Velocidade (m/s para km/h)")
         print("0. Sair")
 
         escolha = input("Digite sua escolha: ")
@@ -26,43 +30,33 @@ def main():
 
         if escolha == "1":
             metros = float(input("Digite o valor em metros: "))
-            quilometros = None
+            quilometros = comprimento.conversao_comprimentroM_km(metros)
             print(f"{metros} metros são {quilometros} quilômetros.")
             
         elif escolha == "2":
             quilometros = float(input("Digite o valor em quilômetros: "))
-            metros = None
+            metros = comprimento.conversao_comprimentroKM_m(quilometros)
             print(f"{quilometros} quilômetros são {metros} metros.")
 
         elif escolha == "3":
             gramas = float(input("Digite o valor em gramas: "))
-            quilogramas = None
+            quilogramas = massa.converter_massa_g_kg(gramas)
             print(f"{gramas} gramas são {quilogramas} quilogramas.")
            
         elif escolha == "4":
             quilogramas = float(input("Digite o valor em quilogramas: "))
-            gramas = None
+            gramas = massa.converter_massa_kg_g(quilogramas)
             print(f"{quilogramas} quilogramas são {gramas} gramas.")
 
         elif escolha == "5":
             litros = float(input("Digite o valor em litros: "))
-            mililitros = None
+            mililitros = volume.converter_milimetro(litros)
             print(f"{litros} litros são {mililitros} mililitros.")
 
         elif escolha == "6":
             mililitros = float(input("Digite o valor em mililitros: "))
-            litros = None
+            litros = volume.converter_litro(mililitros)
             print(f"{mililitros} mililitros são {litros} litros.")
-
-        elif escolha == "7":
-            kmph = float(input("Digite o valor em km/h: "))
-            mps = None
-            print(f"{kmph} km/h são {mps} m/s.")
-
-        elif escolha == "8":
-            mps = float(input("Digite o valor em m/s: "))
-            kmph = None
-            print(f"{mps} m/s são {kmph} km/h.")
 
         else:
             print("Escolha inválida. Por favor, tente novamente.")
